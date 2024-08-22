5d213b455a2f3f697751ada9615f601cb80bcb57
MosesChisz <mosesc@cortexware.co.za>
2024-08-22 15:49:03 +0200

    submit form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
  
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -6,10 +6,11 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
-import SubmitButton from "./SubmitButton"
+import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 // import { Input } from "@/components/ui/input"
 
 export enum FormFieldType {
@@ -26,7 +27,7 @@ export enum FormFieldType {
  
 const PatientForm = () => {
     const router = useRouter();
-    const [isLoading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
   // 1. Define your form.
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
@@ -39,18 +40,17 @@ const PatientForm = () => {
  
   // 2. Define a submit handler.
   async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setLoading(true);
+    setIsLoading(true);
     try {
-        // const userData = { name, email, phone, };
+        const userData = { name, email, phone };
 
-        // const user = await createUser(userData);
+        const user = await createUser(userData);
 
-        // if(user) router.push(`/patients/${user.$id}/register`)
+        if(user) router.push(`/patients/${user.$id}/register`)
     } catch (error) {
         console.log(error);
     }
+    // setIsLoading(false);
   }
   return (
     <Form {...form}>
